8f07564f7eebaa9b6598cf29f4cc1ca9035ff910
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 01:26:23 +0100

    Create owner.js

---
+++ owner.js
@@ -0,0 +1,13 @@
+
+
+```js
+// owner.js
+const OWNER_NUMBER = '243905526836@c.us'; // Ton numéro WhatsApp formaté
+
+function isOwner(sender) {
+  return sender === OWNER_NUMBER;
+}
+
+module.exports = { OWNER_NUMBER, isOwner };
+```
+
