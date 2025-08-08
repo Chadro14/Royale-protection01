b7ebc361ebd1438259d7b89eaafaa50fe95cfb87
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 17:59:36 +0100

    Create index.js

---
+++ service/index.js
@@ -0,0 +1,8 @@
+js
+module.exports = {
+  name: "service",
+  description: "Service client automatique.",
+  execute(client, message) {
+    message.reply("📞 Service client : contactez Son Altesse 🌹 au +243905526836.");
+  }
+}
