50565e809453de862178c6e184457b968a1a4e9f
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 18:32:05 +0100

    Update antilink.js

--- commands/antilink.js
+++ commands/antilink.js
@@ -10,3 +10,22 @@ module.exports = {
     }
   }
 }
+
+js
+const config = require('./config');
+
+module.exports = {
+  name: 'antilink',
+  execute(m, args) {
+    const option = args[0];
+    if (option === 'on') {
+      config.setOption('antilink', true);
+      return m.reply('🛡️ Antilink activé.\n\n_By Altesse 🌹_');
+    } else if (option === 'off') {
+      config.setOption('antilink', false);
+      return m.reply('⚠️ Antilink désactivé.\n\n_By Altesse 🌹_');
+    } else {
+      return m.reply('Utilise `.antilink on` ou `.antilink off`\n\n_By Altesse 🌹_');
+    }
+  }
+};
