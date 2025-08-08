e54efafea10620c7123dd108a8110c1d092261a5
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 18:28:53 +0100

    Update antivirus.js

--- commands/antivirus.js
+++ commands/antivirus.js
@@ -9,3 +9,22 @@ module.exports = {
     }
   }
 }
+
+js
+const config = require('./config');
+
+module.exports = {
+  name: 'antivirus',
+  execute(m, args) {
+    const option = args[0];
+    if (option === 'on') {
+      config.setOption('antivirus', true);
+      return m.reply('🛡️ Antivirus activé.\n\n_By Altesse 🌹_');
+    } else if (option === 'off') {
+      config.setOption('antivirus', false);
+      return m.reply('⚠️ Antivirus désactivé.\n\n_By Altesse 🌹_');
+    } else {
+      return m.reply('Utilise `.antivirus on` ou `.antivirus off`\n\n_By Altesse 🌹_');
+    }
+  }
+};
