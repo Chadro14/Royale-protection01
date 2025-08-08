2344f562f8b219b68f5c4a1332481bf8d0f3b2b3
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 18:30:23 +0100

    Update antihack.js

--- commands/antihack.js
+++ commands/antihack.js
@@ -8,3 +8,22 @@ module.exports = {
     }
   }
 }
+
+js
+const config = require('./config');
+
+module.exports = {
+  name: 'antihack',
+  execute(m, args) {
+    const option = args[0];
+    if (option === 'on') {
+      config.setOption('antihack', true);
+      return m.reply('🛡️ Antihack activé.\n\n_By Altesse 🌹_');
+    } else if (option === 'off') {
+      config.setOption('antihack', false);
+      return m.reply('⚠️ Antihack désactivé.\n\n_By Altesse 🌹_');
+    } else {
+      return m.reply('Utilise `.antihackon` ou `.antihack off`\n\n_By Altesse 🌹_');
+    }
+  }
+};
