9234156695a9756129e2edf582ffd884eed6f35f
Altesse Royal <altessemwamboko@gmail.com>
2025-08-08 17:33:07 +0100

    Create commands.js

---
+++ commands/commands.js
@@ -0,0 +1,11 @@
+js
+module.exports = {
+  antispam: require('./antispam'),
+  antivirus: require('./antivirus'),
+  antiban: require('./antiban'),
+  antilink: require('./antilink'),
+  antihack: require('./antihack'),
+  service: require('./service'),
+  adduser: require('./adduser'),
+  statut: require('./statut')
+}
